3beaad7ad0dc02088d676036df37364ab7fa3863
Madison <mlinkenmeyer@gmail.com>
2023-09-11 18:59:46 -0700

    sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        sequence = [0, 1]
+        while len(sequence) < length:
+            next_number = sequence[-1] + sequence[-2]
+            sequence.append(next_number)
+        print(sequence)
